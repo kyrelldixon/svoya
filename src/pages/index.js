1b72512f9260695317cf89f2efd6abf4ae208012
Kyrell Dixon <kyrell.brown@gmail.com>
2019-07-18 22:36:51 -0500

    Style corner items

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,16 @@
 import React from "react"
+
+import Layout from "../components/layout"
+
 import "../global-styles.css"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <Layout>
+    <div className="h-screen flex flex-wrap flex-col content-between">
+      <div className="flex-50 p-4">top-left</div>
+      <div className="p-4">bottom-left</div>
+      <div className="flex-50 text-right p-4">top-right</div>
+      <div className="text-right p-4">bottom-right</div>
+    </div>
+  </Layout>
+)
