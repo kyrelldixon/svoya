5dfb30ef92df846e8c471c73f1d42919e2c83403
Kyrell Dixon <kyrell.brown@gmail.com>
2019-07-21 17:58:03 -0500

    Move menu-button to component

--- src/components/main-content.js
+++ src/components/main-content.js
@@ -3,6 +3,7 @@ import React from "react"
 import Layer from "./layer"
 // import Nav from "./nav"
 import Overlay from "./overlay"
+import MenuButton from "./menu-button"
 
 const MainContent = () => (
   <section>
@@ -19,10 +20,7 @@ const MainContent = () => (
       <div className="h-screen w-screen flex flex-col justify-between items-start px-4 pt-4 pb-8">
         <div className="w-full flex justify-between items-start leading-none">
           <p className="font-bold">Logo</p>
-          <button className="w-8 flex flex-col items-end">
-            <span className="block w-full border-white border-t-2 pb-1"></span>
-            <span className="block w-2/3 border-white border-b-2"></span>
-          </button>
+          <MenuButton />
         </div>
         <div className="leading-snug">
           <h2 className="text-gray-100 text-xs">Interior</h2>
---
+++ src/components/menu-button.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const MenuButton = () => (
+  <button className="w-8 flex flex-col items-end">
+    <span className="block w-full border-white border-t-2 pb-1"></span>
+    <span className="block w-2/3 border-white border-b-2"></span>
+  </button>
+)
+
+export default MenuButton
