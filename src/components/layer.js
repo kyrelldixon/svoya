e677374b8516c354082bf05afc6b695da18e8c8e
Kyrell Dixon <Kyrell.Dixon@halliburton.com>
2019-07-22 16:00:05 -0500

    Add layering and update z-index

--- src/components/layer.js
+++ src/components/layer.js
@@ -1,9 +1,18 @@
 import React from "react"
 
-const Layer = ({ children }) => (
-  <div className={`h-screen absolute inset-0`}>
-    { children }
-  </div>
-)
+const Layer = ({ children, level = 0 }) => {
+  const levels = {
+    0: 'z-0',
+    1: 'z-10',
+    2: 'z-20',
+    5: 'z-50'
+  }
+  return (
+    <div className={`h-screen absolute inset-0 ${levels[level]}`}>
+      { children }
+    </div>
+  )
+}
+
 
 export default Layer
--- src/components/main-content.js
+++ src/components/main-content.js
@@ -12,27 +12,27 @@ const MainContent = () => {
 
   return (
     <section>
-      <Layer>
+      <Layer level={0}>
         <img
           src="./apartment-architecture-art-276724.jpg"
           alt="An apartment"
           className="object-cover h-full w-screen" />
       </Layer>
-      <Layer>
+      <Layer level={1}>
         <Overlay />
       </Layer>
-      <Layer>
+      <div>
         <div className="h-screen w-screen flex flex-col justify-between items-start px-6 pt-6 pb-8">
-          <div className="w-full flex justify-between items-start leading-none z-10">
+          <div className="w-full flex justify-between items-start leading-none z-50">
             <p className="font-bold">Logo</p>
             <MenuButton toggle={toggle} />
           </div>
-          <div className="leading-snug">
+          <div className="leading-snug z-10">
             <h2 className="text-gray-100 text-xs">Interior</h2>
             <h1 className="text-4xl font-semibold">The Apartment</h1>
           </div>
         </div>
-      </Layer>
+      </div>
       <Menu isActive={isActive} />
     </section>
   )
