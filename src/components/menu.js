f42e632d80430d043e35db2d7d8ef3134aa9e769
Kyrell Dixon <kyrell.brown@gmail.com>
2019-07-21 19:08:08 -0500

    Add styled menu content

--- src/components/menu.js
+++ src/components/menu.js
@@ -3,9 +3,25 @@ import Layer from "./layer";
 
 const Menu = ({ isActive }) => (
   <Layer>
-    <div className={`${isActive ? "flex" : "hidden"} h-screen w-screen bg-gray-800 px-4 pt-24 flex-col justify-between items-start`}>
-      Menu
-    </div>
+    <nav className={`${isActive ? "block" : "hidden"} h-screen w-screen bg-gray-800 px-6 pt-48`}>
+      <ul className="text-3xl text-gray-400 pb-16 leading-relaxed">
+        <li className="text-4xl text-white font-semibold">All Projects</li>
+        <li>Architecture</li>
+        <li>Industrial Design</li>
+        <li>Interior</li>
+      </ul>
+      <ul className="leading-loose">
+        <li>Press</li>
+        <li>About Studio</li>
+        <li>Awards</li>
+        <li>Contact</li>
+      </ul>
+      <ul className="w-full flex justify-end">
+        <li className="pr-4">F</li>
+        <li className="pr-4">I</li>
+        <li>T</li>
+      </ul>
+    </nav>
   </Layer>
 )
 
