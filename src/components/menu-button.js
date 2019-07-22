bdcc61ce27290b8be9dabb40aff7858413c65cd0
Kyrell Dixon <Kyrell.Dixon@halliburton.com>
2019-07-22 15:59:31 -0500

    Fix menu button bug where span not displayed

--- src/components/menu-button.js
+++ src/components/menu-button.js
@@ -1,10 +1,10 @@
 import React from "react"
 
 const MenuButton = ({ toggle }) => (
-  <button onClick={toggle} className="w-8 flex flex-col items-end">
+  <div role="button" onClick={toggle} className="w-8 flex flex-col items-end">
     <span className="block w-full border-white border-t-2 pb-1"></span>
     <span className="block w-2/3 border-white border-b-2"></span>
-  </button>
+  </div>
 )
 
 export default MenuButton
