903a22036eff1401d99f4a00124801fa60b31f4a
Kyrell Dixon <kyrell.brown@gmail.com>
2019-07-23 07:13:14 -0500

    Fix bug: menu button hidden on menu active

--- src/components/menu.js
+++ src/components/menu.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layer from "./layer";
 
 const Menu = ({ isActive }) => (
-  <Layer hidden={!isActive} level={2}>
+  <Layer hidden={!isActive} level={1}>
     <nav className={`h-screen w-screen bg-gray-800 px-6 pt-48`}>
       <ul className="text-3xl text-gray-400 pb-16 leading-relaxed">
         <li className="text-4xl text-white font-semibold">All Projects</li>
