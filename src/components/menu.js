56248a64468cb8b437ad91f77472bc53802868eb
Kyrell Dixon <kyrell.brown@gmail.com>
2019-07-23 07:10:58 -0500

    Animate menu on enter and leave

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,23 +1,33 @@
 import React from "react"
-import Layer from "./layer";
+import { animated, useTransition } from "react-spring";
 
-const Menu = ({ isActive }) => (
-  <Layer hidden={!isActive} level={2}>
-    <nav className={`h-screen w-screen bg-gray-800 px-6 pt-48`}>
-      <ul className="text-3xl text-gray-400 pb-16 leading-relaxed">
-        <li className="text-4xl text-white font-semibold">All Projects</li>
-        <li>Architecture</li>
-        <li>Industrial Design</li>
-        <li>Interior</li>
-      </ul>
-      <ul className="leading-loose pb-8">
-        <li>Press</li>
-        <li>About Studio</li>
-        <li>Awards</li>
-        <li>Contact</li>
-      </ul>
-    </nav>
-  </Layer>
-)
+const Menu = ({ isActive }) => {
+  const transitions = useTransition(isActive, null, {
+    from: { transform: 'translateY(100%)' },
+    enter: { transform: 'translateY(0%)' },
+    leave: { transform: 'translateY(100%)' },
+  })
+
+  return transitions.map(({ item, props, key }) => (
+      item && <animated.div className={`h-screen fixed inset-0 z-10`} style={props}>
+      <nav className={`h-screen w-screen bg-gray-800 px-6 pt-48`}>
+        <ul className="text-3xl text-gray-400 pb-16 leading-relaxed">
+          <li className="text-4xl text-white font-semibold">All Projects</li>
+          <li>Architecture</li>
+          <li>Industrial Design</li>
+          <li>Interior</li>
+        </ul>
+        <ul className="leading-loose pb-8">
+          <li>Press</li>
+          <li>About Studio</li>
+          <li>Awards</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </animated.div>
+    )
+  )
+}
+
 
 export default Menu
