5a8e2fcaad45bb725fbe1d4bb2e6e309e4a6856d
Kyrell Dixon <kyrell.brown@gmail.com>
2019-07-23 20:51:37 -0500

    Use useFullHeight hook with main content

--- src/components/main-content.js
+++ src/components/main-content.js
@@ -5,16 +5,18 @@ import MenuButton from "./menu-button"
 import Menu from "./menu"
 
 import useToggle from "../hooks/useToggle"
+import useFullHeight from "../hooks/useFullHeight"
 
 const MainContent = () => {
   const [isActive, toggle] = useToggle()
+  const height = useFullHeight()
 
   return (
     <section>
       <Layer image="./apartment-architecture-art-276724.jpg" />
       <Layer hidden image="./apartment-clean-condominium-279719.jpg" />
-      <div>
-        <div className="h-screen w-screen flex flex-col justify-between items-start px-6 pt-6 pb-8">
+      <div style={{ height }}>
+        <div className="h-full w-screen flex flex-col justify-between items-start px-6 pt-6 pb-8">
           <div className="w-full flex justify-between items-start leading-none z-50">
             <p className="font-bold">Logo</p>
             <MenuButton toggle={toggle} />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 import React from "react"
 
 import Layout from "../components/layout"
-import MobileHeight from "../components/mobile-height"
+import MainContent from "../components/main-content"
 
 import "../global-styles.css"
 
 export default () => (
   <Layout>
-    <MobileHeight />
+    <MainContent />
   </Layout>
 )
