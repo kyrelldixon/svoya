07f6b26d9436e7f16e749bc9d92681cdafc3e9be
Kyrell Dixon <kyrell.brown@gmail.com>
2019-07-23 19:40:07 -0500

    Add component for testing mobile height

---
+++ src/components/mobile-height.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const MobileHeight = () => (
+  <div className="h-screen flex flex-col justify-center items-center">
+    <p>This element should be centered</p>
+  </div>
+)
+
+export default MobileHeight
